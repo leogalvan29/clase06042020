84c7a6f466d4231ea62bba153a919b4801bf0661
leogalvan28 <leonelgalvanlobato@gmail.com>
2020-04-06 20:42:48 -0500

    primer version

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+function suma(a,b){
+    console.log(a+b)
+}
+
+suma(7,9);
+
+
+const restar =(c,d)=>{
+    console.log(c-d)
+}
+
+console.log(restar(189,69))
+
+let animales = [1,2,3,1,2,3,1,2,3]
+
+
+
+const multiplicar = (x,y) =>{
+    console.log(x*y)
+}
+
+multiplicar(89,55)
+
+const areaTriangulo = (base,altura,divi) =>{
+    console.log(base*altura/divi)
+}
+
+areaTriangulo(75,23,2)
+
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+
